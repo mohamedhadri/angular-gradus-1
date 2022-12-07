498f2580fa0fc9d97a35dd5ef42fdc2418f2bcf2
Mohamed Hadri <mhadri1000@gmail.com>
2022-12-07 10:07:06 +0300

    added form

--- src/app/muhammad/forms-test/forms-test.module.ts
+++ src/app/muhammad/forms-test/forms-test.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsTestComponent } from './reactive-forms-test/reactive-forms-test.component';
-import {ReactiveFormsModule} from "@angular/forms";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {RouterModule, Routes} from "@angular/router";
 import {ProfileComponent} from "../../pages/profile/profile.component";
 import {SharedModule} from "../../shared/shared.module";
-
+import { HttpClientModule } from '@angular/common/http';
+import {MatDatepickerModule} from '@angular/material/datepicker';
 export const routes: Routes = [
   { path: 'reactive-form', component: ReactiveFormsTestComponent, data: { breadcrumb: 'reactive-form' } },
 ];
@@ -15,6 +16,9 @@ export const routes: Routes = [
     ReactiveFormsTestComponent
   ],
     imports: [
+      MatDatepickerModule,
+      FormsModule,
+      HttpClientModule,
         CommonModule,
         ReactiveFormsModule,
       RouterModule.forChild(routes),
--- src/app/muhammad/forms-test/reactive-forms-test/reactive-forms-test.component.ts
+++ src/app/muhammad/forms-test/reactive-forms-test/reactive-forms-test.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
 
@@ -6,27 +7,64 @@ import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
   templateUrl: './reactive-forms-test.component.html',
   styleUrls: ['./reactive-forms-test.component.css']
 })
+
 export class ReactiveFormsTestComponent implements OnInit {
+  date = new FormControl(new Date());
+  serializedDate = new FormControl(new Date().toISOString());
+  constructor(private fb: FormBuilder, private http: HttpClient) { }
+  selectedCar: String = "Category";
+  carsData:any;
+  models: Array<any> = []; 
+	pickups: Array<any> = [];  
+	dropoff: Array<any> = [];  
   form: FormGroup;
-  model: any
-  constructor(private fb: FormBuilder) { }
+
+  changeCar(newCar: any) { 
+		this.models = this.carsData.find((car: any) => car.name == newCar.value).models; 	
+    
+	}
+  changeModel(state: any) { 
+		this.pickups = this.carsData
+		.find((car: any) => car.name == this.selectedCar).pickup;
+		this.dropoff = this.carsData
+		.find((car: any) => car.name == this.selectedCar).dropoff;
+  
+	}
+  
 
   ngOnInit(): void {
+    const url:string='../../../../assets/data/cars.json';
+    this.http.get(url).subscribe((response)=>{
+      this.carsData=response
+      
+    })
     this.creatForm();
     this.setFormValue();
   }
 
   save(){
-    this.getFormValue();
-    console.log(this.form);
+    // this.getFormValue();
+    console.log(this.form.value);
   }
 
+  dropoffDate;
   private creatForm() {
     this.form = this.fb.group({
-      name: ['', Validators.required],
-      body: ['', Validators.maxLength(10)],
+      // name: ['', Validators.required],
+      // body: ['', Validators.maxLength(10)],
+      clientCar:['', Validators.required],
+      clientModel:['', Validators.required],
+      clientPickup:['', Validators.required],
+      pickupDate:['', Validators.required],
+      clientDropoff:['', Validators.required],
+      dropoffDate:['', Validators.required],
+      clientName:['', Validators.required],
+      clientEmail:['', Validators.required],
+      clientPhone:['', Validators.required],
+      
     })
   }
+  model: any;
   getFormValue() {
     this.model = {
       name: this.form.controls.name.value,
@@ -38,11 +76,12 @@ export class ReactiveFormsTestComponent implements OnInit {
       name: 'gggg',
       body: 'hhhh',
     })*/
-    this.form.patchValue({
-      name: 'gggg',
-    })
+    // this.form.patchValue({
+    //   name: 'gggg',
+    // })
     // this.form.controls.name.setValue('ddddddddd')
 
     console.log(this.model)
   }
+  
 }
